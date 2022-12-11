ae8efff169081580de4f41de30ec350b867cf4c4
miha <gomihagle@gmail.com>
2022-12-12 00:00:16 +0200

    added certicate, helmet,and password lookup

--- app.js
+++ app.js
@@ -5,17 +5,21 @@ const http = require('http')
 const https = require('https')
 const fs = require('fs')
 const mongoose = require('mongoose')
+const helmet = require('helmet')
 
 HTTP_PORT = config.get('http_port')
 HTTPS_PORT = config.get('https_port')
 
 const app = express()
 
+app.use(require('./middleware/middleware.https-redirect'))
+app.use(helmet())
 app.use(express.static(path.join(__dirname, '/client')))
 app.use('/node_modules', express.static(path.join(__dirname,'/node_modules')))
 app.use('/api/auth',require('./routes/routes.auth'))
 app.use('/api/data',require('./routes/routes.data'))
 app.use('/',require('./routes/routes.pages'))
+
 
 
 async function start() {
@@ -29,8 +33,8 @@ async function start() {
         http.createServer(app).listen(HTTP_PORT, () => {console.log(`Server listening port ${HTTP_PORT}`);})
 
         https.createServer({
-            key: fs.readFileSync('hkeys/key.pem'),
-            cert: fs.readFileSync('hkeys/cert.pem')
+            key: fs.readFileSync('./sslcert/privkey.pem'),
+            cert: fs.readFileSync('./sslcert/fullchain.pem')
         },app).listen(HTTPS_PORT, () => {console.log(`Server listening port ${HTTPS_PORT}`);})
 
     } catch (e) {
